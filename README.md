15bba7bc1de1ea767583c5d322a63721bf25b350
Evelyne R <evelyneregterschot@outlook.com>
2024-06-28 09:33:14 +0000

    test: connect to github again

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 # FCC_Periodic_Table_Database
 FreeCodeCamp SQL/Bash/Git project: Build a Periodic Table Database
 =======
 Periodic Table Database
 >>>>>>> 3c504f5 (Initial commit)
+=======
+Periodic Table Database
+Test
+>>>>>>> eaca478 (test: connect to github)
--- element.sh
+++ element.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+# Get data from the table
+ELEMENT=$($PSQL <<EOF
+      SELECT e.name, e.symbol, e.atomic_number, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
+      FROM elements e
+      JOIN properties p ON e.atomic_number = p.atomic_number
+	JOIN types t ON p.type_id = t.type_id
+      WHERE e.atomic_number = '$1' OR e.name = '$1' OR e.symbol = '$1';
+    EOF)
